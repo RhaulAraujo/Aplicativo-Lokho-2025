f32b51a16f7019e8d32fb515d539c7eac5251605
mayara <mayara.santana46@etec.sp.gov.br>
2025-04-14 17:01:07 -0300

    fazendo página inicial

--- app/tabs/explore.tsx
+++ app/tabs/explore.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, Platform, View, Text, TouchableOpacity, Button, ViewStyle } from 'react-native';
 import { FontAwesome } from "@expo/vector-icons";
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -7,31 +7,66 @@ import { Dimensions } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import React, {useState} from 'react';
 
 export default function TabTwoScreen() {
-  return (
+      const [progress, setProgress] = useState(0);
 
+      const handlePress = () => {
+        if (progress < 3) {
+          setProgress(progress + 1);
+        }
+      };
+
+      return (
       <ThemedView style={styles.container}>
 
       <View style={styles.navBar}>
         <TouchableOpacity style={styles.navItem} onPress={() => console.log('Opção 1')}>
           <FontAwesome name="home" size={24} color="black" />
         </TouchableOpacity>
-        <Text style={styles.navTitle}>Explorar</Text>
+        <Text style={styles.navTitle}></Text>
         <TouchableOpacity style={styles.navItem} onPress={() => console.log('Opção 2')}>
           <FontAwesome name="search" size={24} color="black" />
         </TouchableOpacity>
       </View>
 
       <View style={styles.nav}>
-        <ThemedText style={styles.titu_tex3}>Módulo 1</ThemedText>
+        <ThemedText style={styles.titu_tex3}>Iniciante</ThemedText>
+        <View style={styles.progressBarContainer}>
+        <View style={[styles.progressBar, 
+          { 
+            backgroundColor: progress >= 3 ? 'blue' : 'blue',
+            width: `${progress * 33.33}%`,
+            } as ViewStyle, 
+          ]} 
+          />
+        </View>
+        <Button title="Aumentar" onPress={handlePress} />
       </View>
 
         <ThemedView style={styles.boxtop}>
-          <ThemedText style={styles.titu_tex1}>Explore</ThemedText>
-        </ThemedView> 
-        <ThemedView style={styles.boxMid}>
-          <ThemedText style={styles.titu_tex}>Explore</ThemedText>
+          <ThemedText style={styles.titu_tex1}>Módulo 1</ThemedText>
+            <ThemedText style={styles.subtitu_tex1}>0/5 Atividades</ThemedText>
+              <View style={styles.containeractiv}>
+                <Image style={styles.Foto_t} source={require('@/assets/images/lbs2.jpg')} resizeMode='contain'></Image>
+                <TouchableOpacity style={styles.navItem} onPress={() => console.log('Opção 1')}>   
+                  <ThemedText style={styles.foto_texto}>Alfabeto</ThemedText>
+                  </TouchableOpacity>
+              </View>
+              <View style={styles.containeractiv}>
+              <Image style={styles.Foto_t} source={require('@/assets/images/lbs2.jpg')} resizeMode='contain'></Image>
+              <ThemedText style={styles.foto_texto2}>Cumprimentos</ThemedText>
+              </View>
+              <View style={styles.containeractiv}>
+              <Image style={styles.Foto_t} source={require('@/assets/images/lbs2.jpg')} resizeMode='contain'></Image>
+              <ThemedText style={styles.foto_texto3}>Soletrando</ThemedText>
+              </View>
+              <View style={styles.containeractiv}>
+              <Image style={styles.Foto_t} source={require('@/assets/images/lbs2.jpg')} resizeMode='contain'></Image>
+              <ThemedText style={styles.foto_texto4}>Frases</ThemedText>
+              </View>
+   
         </ThemedView>
       </ThemedView>
 
@@ -55,13 +90,25 @@ const styles = StyleSheet.create({
  //   alignItems: 'center',
    // justifyContent: 'center',
   },
+  progressBarContainer: {
+    width: '60%',
+    height: 20,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  progressBar: {
+    height: '100%',
+  },
   boxtop: {
    // backgroundImage: 'linear-gradient(to bottom,rgb(190, 103, 4) 0%,rgb(48, 27, 1) 51%)',
    backgroundColor:  '#be6704',
    height: Dimensions.get('window').height/2,
     width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    marginLeft: 5,
   },
   boxMid: {
     backgroundColor: 'linear-gradient(to bottom,rgb(190, 103, 4) 0%,rgb(48, 27, 1) 51%)',
@@ -79,15 +126,21 @@ const styles = StyleSheet.create({
   },
   titu_tex3: {
     color: '#000',
-    fontSize: 30,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 40,
+    marginTop: 5,
   },
   titu_tex1: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
-    marginTop: 40,
+    marginTop: 20,
+  },
+  subtitu_tex1: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 5,
   },
   nav: {
     width: '100%',
@@ -114,5 +167,39 @@ const styles = StyleSheet.create({
       fontSize: 18,
       fontWeight: 'bold',
       color: 'black',
+    },
+    Foto_t: {
+      marginTop: 15,
+      height: 90,
+      width: 90,
+    },
+    foto_texto: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 5,
+    },
+    foto_texto2: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 5,
+    },
+    foto_texto3: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 5,
+    },
+    foto_texto4: {
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginTop: 5,
+    },
+    containeractiv: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
     },
 });
